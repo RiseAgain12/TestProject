af2773215500ff563e687a2642770987253db249
RAJESH <RAJESH@DESKTOP-OB3MQFV>
2022-12-14 11:20:14 +0530

    Second Commit

---
+++ TestProject/src/entity/Product.java
@@ -0,0 +1,29 @@
+package entity;
+
+public class Product {
+private int pid;
+private String name;
+
+public Product(int pid, String name) {
+	this.pid = pid;
+	this.name = name;
+}
+
+public int getPid() {
+	return pid;
+}
+
+public void setPid(int pid) {
+	this.pid = pid;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+	
+}
